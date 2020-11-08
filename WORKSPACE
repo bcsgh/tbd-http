3c26253a7407f367b679b562ff78507799c8bcbc
bcsgh <33939446+bcsgh@users.noreply.github.com>
2020-11-07 19:50:01 -0800

    Add an embedded static page.

--- WORKSPACE
+++ WORKSPACE
@@ -5,7 +5,7 @@ load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
 
 git_repository(
     name = "rules_foreign_cc",
-    commit = "d54c78ab86b40770ee19f0949db9d74a831ab9f0",  # current as of 2020/10/32
+    commit = "d54c78ab86b40770ee19f0949db9d74a831ab9f0",  # current as of 2020/11/07
     remote = "git://github.com/bazelbuild/rules_foreign_cc.git",
 )
 
@@ -27,6 +27,21 @@ git_repository(
     commit = "c8f8135a5720aee7de8328b42e4c43f8aa2e60aa",  # current as of 2020/11/07
     shallow_since = "1601711556 +0200",
     remote = "git://github.com/google/glog.git",
+)
+
+#############################################
+git_repository(
+    name = "com_google_absl",
+    commit = "e96d49687d9c078f2d47356b6723c3b5715493f7",  # current as of 2020/11/07
+    shallow_since = "1604603876 -0500",
+    remote = "git://github.com/abseil/abseil-cpp.git",
+)
+
+#############################################
+git_repository(
+    name = "bazel_rules",
+    commit = "d2dcb62093486e3c920b6a37c3835ca33dc247bc",  # current as of 2020/11/07
+    remote = "git://github.com/bcsgh/bazel_rules.git",
 )
 
 #############################################
--- server/tbd.cc
+++ server/tbd.cc
@@ -4,12 +4,29 @@
 
 #include <httpserver.hpp>
 
+#include "server/static_embed_emebed_data.h"
+
 namespace tbd_server {
 using httpserver::http_request;
 using httpserver::http_response;
 using httpserver::http::http_utils;
 
 namespace {
+class Static : public httpserver::http_resource {
+ public:
+  Static(const std::string data, const std::string mime) :
+    data_(std::move(data)),
+    mime_(std::move(mime)) {}
+
+  const std::shared_ptr<http_response> render(const http_request&) override {
+    return std::shared_ptr<http_response>{new httpserver::string_response(
+        data_, http_utils::http_ok, mime_)};
+  }
+
+ private:
+  const std::string data_, mime_;
+};
+
 class TbdServer_JSON : public httpserver::http_resource {
  public:
   const std::shared_ptr<http_response> render(const http_request&) override;
@@ -25,13 +42,18 @@ const std::shared_ptr<http_response> TbdServer_JSON::render(const http_request& 
 
 struct TbdServer::Impl {
   TbdServer_JSON json;
+  Static html;
+
+  Impl() :
+    html(std::string{server_main_html()}, "text/html") {}
 };
 
 TbdServer::TbdServer() : impl_(new TbdServer::Impl) {}
 TbdServer::~TbdServer() = default;
 
 void TbdServer::RegisterResources(httpserver::webserver *ws) {
-  ws->register_resource("/hello", &impl_->json, true);
+  ws->register_resource("/", &impl_->html, true);
+  ws->register_resource("/json", &impl_->json, true);
 };
 
 } // namespace tbd_server
