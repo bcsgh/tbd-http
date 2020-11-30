32198bebf9d9383e2f1e797b6374e3f14700c0d3
bcsgh <33939446+bcsgh@users.noreply.github.com>
2020-11-29 19:20:46 -0800

    Add smoke test for //server:tbd_js.

---
+++ server/tbd_test.js
@@ -0,0 +1,12 @@
+goog.module('TBD.Main_test');
+goog.setTestOnly('TBD.Main_test');
+
+var Main = goog.require('TBD.Main');
+var testSuite = goog.require('goog.testing.testSuite');
+
+goog.require('goog.testing.jsunit');
+
+testSuite({
+  testSmoke: function() {
+  }
+});
