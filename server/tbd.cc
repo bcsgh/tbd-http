9788ebf7693809ee77607781a1492708033c3d8e
bcsgh <33939446+bcsgh@users.noreply.github.com>
2020-11-30 19:51:43 -0800

    Move some stuff into a impl header to enable testing.

--- server/tbd.cc
+++ server/tbd.cc
@@ -12,6 +12,7 @@
 #include "glog/logging.h"
 #include "json/json.h"
 #include "server/static_embed_emebed_data.h"
+#include "server/tbd_impl.h"
 #include "tbd/preamble_emebed_data.h"
 #include "tbd/tbd.h"
 
@@ -24,26 +25,7 @@ using httpserver::http_request;
 using httpserver::http_response;
 using httpserver::http::http_utils;
 
-namespace {
-class Static : public httpserver::http_resource {
- public:
-  Static(const std::string data, const std::string mime) :
-    data_(std::move(data)),
-    mime_(std::move(mime)) {}
-
-  const std::shared_ptr<http_response> render(const http_request&) override {
-    return std::shared_ptr<http_response>{new httpserver::string_response(
-        data_, http_utils::http_ok, mime_)};
-  }
-
- private:
-  const std::string data_, mime_;
-};
-
-class TbdServer_JSON : public httpserver::http_resource {
- public:
-  const std::shared_ptr<http_response> render(const http_request&) override;
-};
+namespace impl {
 
 class Sink : public tbd::ProcessOutput {
  public:
@@ -121,17 +103,11 @@ const std::shared_ptr<http_response> TbdServer_JSON::render(const http_request& 
       out.str(), code, "application/json")};
 }
 
-}  // namespace
-
-struct TbdServer::Impl {
-  TbdServer_JSON json;
-  Static html, js, preamble;
-
-  Impl() :
+}  // namespace impl
+TbdServer::Impl::Impl() :
     html(std::string{server_main_html()}, "text/html"),
     js(std::string{server_tbd_main_js_js()}, "text/javascript"),
     preamble(std::string{::tbd_preamble_tbd()}, "text/x.tbd") {}
-};
 
 TbdServer::TbdServer() : impl_(new TbdServer::Impl) {}
 TbdServer::~TbdServer() = default;
---
+++ server/tbd_impl.h
@@ -0,0 +1,47 @@
+#ifndef SERVER_TBD_IMPL_H_
+#define SERVER_TBD_IMPL_H_
+
+#include "server/tbd.h"
+
+#include <string>
+#include <memory>
+
+#include <httpserver.hpp>
+
+namespace tbd_server {
+using httpserver::http_request;
+using httpserver::http_response;
+using httpserver::http::http_utils;
+
+namespace impl {
+class Static : public httpserver::http_resource {
+ public:
+  Static(const std::string data, const std::string mime) :
+    data_(std::move(data)),
+    mime_(std::move(mime)) {}
+
+  const std::shared_ptr<http_response> render(const http_request&) override {
+    return std::shared_ptr<http_response>{new httpserver::string_response(
+        data_, http_utils::http_ok, mime_)};
+  }
+
+ private:
+  const std::string data_, mime_;
+};
+
+class TbdServer_JSON : public httpserver::http_resource {
+ public:
+  const std::shared_ptr<http_response> render(const http_request&) override;
+};
+}  // namespace impl
+
+struct TbdServer::Impl {
+  impl::TbdServer_JSON json;
+  impl::Static html, js, preamble;
+
+  Impl();
+};
+
+} // namespace tbd_server
+
+#endif // SERVER_TBD_IMPL_H_
