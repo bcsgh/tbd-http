2b60138d5d15f198f0f816fc6fba5c7d1f985f85
bcsgh <33939446+bcsgh@users.noreply.github.com>
2020-12-01 21:15:27 -0800

    Add JS unit tests for FillTable.

--- server/tbd_test.js
+++ server/tbd_test.js
@@ -7,6 +7,52 @@ var testSuite = goog.require('goog.testing.testSuite');
 goog.require('goog.testing.jsunit');
 
 testSuite({
-  testSmoke: function() {
-  }
+  testFillTableEmpty: function() {
+    let output = /** @type{HTMLSpanElement} */(document.createElement("span"));
+    output.innerText = "mallory";
+
+    let result = {"values": []};
+
+    Main.impl.FillTable(output, result);
+
+    assertEquals(output.innerText, "Name Value Units ");
+  },
+
+  testFillTableEmptyItems: function() {
+    let output = /** @type{HTMLSpanElement} */(document.createElement("span"));
+    output.innerText = "mallory";
+
+    let result = {"values": [{}, {}]};
+
+    Main.impl.FillTable(output, result);
+
+    assertEquals(output.innerText, "Name Value Units ");
+  },
+
+  testFillTablePartialItems: function() {
+    let output = /** @type{HTMLSpanElement} */(document.createElement("span"));
+    output.innerText = "mallory";
+
+    let result = {"values": [{"unit": "[x]"}, {"value":"123."}, {"name":"bob"}]};
+
+    Main.impl.FillTable(output, result);
+
+    assertEquals(output.innerText, "Name Value Units [x]123.bob");
+  },
+
+  testFillTableFullItems: function() {
+    let output = /** @type{HTMLSpanElement} */(document.createElement("span"));
+    output.innerText = "mallory";
+
+    let result = {"values": [
+      {"name":"alice", "value":"123.", "unit": "[x]"},
+      {"name":"bob",   "value":"45.6", "unit": "[y]"},
+      {"name":"trent", "value":"7.89", "unit": "[z]"},
+    ]};
+
+    Main.impl.FillTable(output, result);
+
+    assertEquals(output.innerText,
+                 "Name Value Units alice123.[x]bob45.6[y]trent7.89[z]");
+  },
 });
